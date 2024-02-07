53bc5a1c150475a51586240f02077ef744984151
nimisha <nbnimisha04@gmail.com>
2024-02-07 23:09:32 +0530

    finally resolved rebase conflict

--- file1.py
+++ file1.py
@@ -4,4 +4,6 @@ print("One more time please guys")
 print("OOOOOOOOOOOOOOOOOOOOOOOOOOOO")
 print("I know you guys can do it")
 print("Please be a merge conflict and i shall leave you")
+print("Just wanted to say thank you for being a merge conflict")
+print("Now moving on to rebase")
 
