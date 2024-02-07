ab4c2d3adcd4413c041b0ce9125e31705847eaae
nimisha <nbnimisha04@gmail.com>
2024-02-07 22:09:13 +0530

    changes in file1.py

--- file1.py
+++ file1.py
@@ -1,6 +1,2 @@
-#somthingya
-name = input("Enter your fathers name; ")
-print("I respect your father,"+name)
-name = input("Enter your name and age: ")
-print("I respect you,"+name+"and your age")
 print("Yes i DO!")
+print("okauyyyyyyyyyy")
