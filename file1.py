06aea98002baf720ba013895357c33937bc43333
nimisha <nbnimisha04@gmail.com>
2024-02-07 23:07:26 +0530

    rebase resolved

--- file1.py
+++ file1.py
@@ -2,3 +2,6 @@ print("I know you guys can do it")
 print("Please be a merge conflict and i shall leave you")
 print("One more time please guys")
 print("OOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+print("I know you guys can do it")
+print("Please be a merge conflict and i shall leave you")
+
